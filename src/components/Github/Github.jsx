7727cbc7a29599b9d822ae3c1bc0bd15aadc1a6e
samrth101 <samthombre91@gmail.com>
2024-07-24 02:14:55 -0700

    some edits

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -3,18 +3,8 @@ import { useEffect } from 'react'
 import {useLoaderData} from 'react-router-dom'
 
 function Github() {
-
+  const data = useLoaderData()
     
-
-    const [data, setData] = React.useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/samarth010')
-        .then((response) => response.json())
-        .then(data => {
-            console.log(data)
-            setData(data)
-        })
-    }, [])
 
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Public Repos: {data.public_repos}
@@ -28,5 +18,5 @@ export default Github
 
 export const githubInfoLoader = async () => {
     const response = await fetch('https://api.github.com/users/samarth010')
-    return response.json()
+    return response.json()  
 }
